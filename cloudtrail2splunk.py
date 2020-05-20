475809cb3fbbef9ab8fcd3c1ebf63348d4c6328b
Bardia Omran <TheRealBards@users.noreply.github.com>
2020-05-20 16:18:54 +0100

    Create cloudtrail2splunk.py

---
+++ cloudtrail2splunk.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import os
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", help="Enter the path to the input file.", type=str, required=True)
+parser.add_argument("--output", help="Enter the path for the output file. This is an optional field.", type=str, required=False)
+
+args = parser.parse_args()
+
+if os.path.exists(args.input):
+    with open(args.input) as f:
+        try:
+            fil = json.loads(f.read())
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+
+if 'Records' in fil.keys():
+    if args.output:
+        fil_name = args.output
+    else:
+        fil_name = args.input+'_parsed'
+
+    with open(fil_name, 'w') as f:
+        for x in fil['Records']:
+            f.write(json.dumps(x))
+            f.write('\n')
+
+
+    print('Successfully written JSON events to {}'.format(fil_name))
